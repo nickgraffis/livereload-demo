55532f07ad03432b1e46257d3e272c6ae8622217
Nick Graffis <nicholasgraffis@gmail.com>
2022-01-08 11:10:38 -0800

    init

---
+++ app.js
@@ -0,0 +1,75 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+
+var livereload = require("livereload");
+var connectLivereload = require("connect-livereload");
+
+
+
+
+
+const exphbs = require('express-handlebars');
+const handlebars = exphbs.create();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const publicDirectory = path.join(__dirname, 'public');
+
+
+
+
+
+var liveReloadServer = livereload.createServer();
+liveReloadServer.watch(publicDirectory);
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+
+
+
+
+
+var app = express();
+app.engine('handlebars', handlebars.engine);
+app.use(connectLivereload());
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(publicDirectory));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+console.log("Starting server");
+
+module.exports = app;
